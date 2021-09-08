7fe3539f72508c35691a5d0fd88e1363eaf77f9d
Pawan Theekshana <87897206+ImTheekshana126@users.noreply.github.com>
2021-09-08 18:44:41 +0530

    Create LisaMusicBot.py

---
+++ LisaMusicBot.py
@@ -0,0 +1,116 @@
+# © Theekshana
+
+from pyrogram import Client, filters
+from pyrogram.types import (
+    InlineKeyboardButton,
+    InlineKeyboardMarkup
+)
+import youtube_dl
+from youtube_search import YoutubeSearch
+import requests
+
+import os
+from config import Config
+
+bot = Client(
+    'Miss-Lisa-Music ',
+    bot_token = Config.BOT_TOKEN,
+    api_id = Config.API_ID,
+    api_hash = Config.API_HASH
+)
+
+## Extra Fns -------------------------------
+
+# Convert hh:mm:ss to seconds
+def time_to_seconds(time):
+    stringt = str(time)
+    return sum(int(x) * 60 ** i for i, x in enumerate(reversed(stringt.split(':'))))
+
+
+## Commands --------------------------------
+@bot.on_message(filters.command(['start']))
+def start(client, message):
+    darkprince = f'👋 Hello @{message.from_user.username}\n\n [🙋‍♀️](https://telegra.ph/file/b3d5d452112b3e09b44fd.jpg)\n I\'m Lisa Music, I can upload songs from YouTube. Type /a song name:'
+    message.reply_text(
+        text=darkprince, 
+        quote=False,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton('BOT Channel', url='https://t.me/Theekshana_Official'),
+                    InlineKeyboardButton('Support Group', url='https://t.me/Theekshana_Support')
+                ]
+                 [
+                    InlineKeyboardButton('➕ Add me to your Group➕', url='https://t.me/MissLisaMusic_bot?startgroup=new'),
+                 ] 
+            ]
+        )
+    )
+
+@bot.on_message(filters.command(['a']))
+def a(client, message):
+    query = ''
+    for i in message.command[1:]:
+        query += ' ' + str(i)
+    print(query)
+    m = message.reply('🔎 Searching the song ...')
+    ydl_opts = {"format": "bestaudio[ext=m4a]"}
+    try:
+        results = []
+        count = 0
+        while len(results) == 0 and count < 6:
+            if count>0:
+                time.sleep(1)
+            results = YoutubeSearch(query, max_results=1).to_dict()
+            count += 1
+        # results = YoutubeSearch(query, max_results=1).to_dict()
+        try:
+            link = f"https://youtube.com{results[0]['url_suffix']}"
+            # print(results)
+            title = results[0]["title"]
+            thumbnail = results[0]["thumbnails"][0]
+            duration = results[0]["duration"]
+
+            ## UNCOMMENT THIS IF YOU WANT A LIMIT ON DURATION. CHANGE 1800 TO YOUR OWN PREFFERED DURATION AND EDIT THE MESSAGE (30 minutes cap) LIMIT IN SECONDS
+            # if time_to_seconds(duration) >= 1800:  # duration limit
+            #     m.edit("Exceeded 30mins cap")
+            #     return
+
+            views = results[0]["views"]
+            thumb_name = f'thumb{message.message_id}.jpg'
+            thumb = requests.get(thumbnail, allow_redirects=True)
+            open(thumb_name, 'wb').write(thumb.content)
+
+        except Exception as e:
+            print(e)
+            m.edit('Found nothing. Try changing the spelling a little.')
+            return
+    except Exception as e:
+        m.edit(
+            "✖️ Found Nothing. Sorry.\n\nTry another keywork or maybe spell it properly."
+        )
+        print(str(e))
+        return
+    m.edit("⏬ Downloading By @TheMizuBot.")
+    try:
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(link, download=False)
+            audio_file = ydl.prepare_filename(info_dict)
+            ydl.process_info(info_dict)
+        rep = f'🎧 **Title**: [{title[:35]}]({link})\n⏳ **Duration**: `{duration}`\n👁‍🗨 **Views**: `{views}`'
+        secmul, dur, dur_arr = 1, 0, duration.split(':')
+        for i in range(len(dur_arr)-1, -1, -1):
+            dur += (int(dur_arr[i]) * secmul)
+            secmul *= 60
+        message.reply_audio(audio_file, caption=rep, parse_mode='md',quote=False, title=title, duration=dur, thumb=thumb_name)
+        m.delete()
+    except Exception as e:
+        m.edit('❌ Error')
+        print(e)
+    try:
+        os.remove(audio_file)
+        os.remove(thumb_name)
+    except Exception as e:
+        print(e)
+
+bot.run()
